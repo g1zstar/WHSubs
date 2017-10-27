18171e0ad07bc7433413b20d996e38fda058a82c
g1zstar <sensiblemagic@gmail.com>
2017-10-27 03:26:05 -0400

    Update WHSubs.lua

--- WHSubs.lua
+++ WHSubs.lua
@@ -3,3 +3,4 @@ sub_alcovex = {["ROGUE"] = 99991231}
 sub_evilgav = {["ROGUE"] = 20171113}
 sub_fooshi  = {["ROGUE"] = 20171113}
 sub_toxin87 = {["ROGUE"] = 20171113}
+sub_zrrx    = {["ROGUE"] = 20171113}
