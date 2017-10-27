5e732af931f90ed77d2fb9781c3aa65c28228b22
g1zstar <sensiblemagic@gmail.com>
2017-10-27 02:34:16 -0400

    Update WHSubs.lua

--- WHSubs.lua
+++ WHSubs.lua
@@ -1,4 +1,5 @@
-sub_g1zstar = {["ALL"] = 99991231}
+--sub_g1zstar = {["ALL"] = 99991231}
 sub_alcovex = {["ROGUE"] = 99991231}
 sub_evilgav = {["ROGUE"] = 20171113}
 sub_fooshi  = {["ROGUE"] = 20171113}
+sub_toxin87 = {["ROGUE"] = 20171113}
