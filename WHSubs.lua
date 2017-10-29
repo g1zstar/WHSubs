987322d4fd9bde96474327c6d7251f740f343510
g1zstar <sensiblemagic@gmail.com>
2017-10-29 09:24:43 -0400

    YAY

--- WHSubs.lua
+++ WHSubs.lua
@@ -1,9 +1,9 @@
-sub_g1zstar = {["ALL"] = 99991231}
-sub_alcovex = {["ROGUE"] = 99991231}
-sub_evilgav = {["ROGUE"] = 20171113}
-sub_fooshi  = {["ROGUE"] = 20171113}
-sub_toxin87 = {["ROGUE"] = 20171113}
-sub_zrrx    = {["ROGUE"] = 20171113}
+sub_g1zstar    = {["ALL"]   = 99991231}
+sub_alcovex    = {["ROGUE"] = 99991231}
+sub_evilgav    = {["ROGUE"] = 20171113}
+sub_fooshi     = {["ROGUE"] = 20171113}
+sub_toxin87    = {["ROGUE"] = 20171113}
+sub_zrrx       = {["ROGUE"] = 20171113}
 sub_poisonling = {["ROGUE"] = 20171114}
-sub_mxrender = {["ROGUE"] = 20171115}
-sub_spacek = {["ROGUE"] = 20171115}
+sub_mxrender   = {["ROGUE"] = 20171115}
+sub_spacek     = {["ROGUE"] = 20171115}
