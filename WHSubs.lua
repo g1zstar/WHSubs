823c6e1c0bec94389003ed9433e5d1746db14e98
g1zstar <sensiblemagic@gmail.com>
2017-11-02 03:45:42 -0400

    CLERICAL ERROR

--- WHSubs.lua
+++ WHSubs.lua
@@ -1,14 +1,14 @@
 sub_g1zstar    = {["DEMONHUNTER"] = 20171114}
 sub_alcovex    = {["ROGUE"] = 99991231}
-sub_evilgav    = {["ROGUE"] = 20171113}
-sub_fooshi     = {["ROGUE"] = 20171113}
-sub_toxin87    = {["ROGUE"] = 20171113}
-sub_zrrx       = {["ROGUE"] = 20171113}
-sub_poisonling = {["ROGUE"] = 20171114}
-sub_mxrender   = {["ROGUE"] = 20171115}
-sub_spacek     = {["ROGUE"] = 20171115}
-sub_inkedqt    = {["ROGUE"] = 20171115}
-sub_binkya     = {["ROGUE"] = 20171117}
+sub_evilgav    = {["ROGUE"] = 20171127}
+sub_fooshi     = {["ROGUE"] = 20171127}
+sub_toxin87    = {["ROGUE"] = 20171127}
+sub_zrrx       = {["ROGUE"] = 20171127}
+sub_poisonling = {["ROGUE"] = 20171128}
+sub_mxrender   = {["ROGUE"] = 20171129}
+sub_spacek     = {["ROGUE"] = 20171129}
+sub_inkedqt    = {["ROGUE"] = 20171129}
+sub_binkya     = {["ROGUE"] = 20171201}
 sub_frontliner1989 = {["ROGUE"] = 20171202}
 sub_neverlucky = {["ROGUE"] = 20171202}
 
