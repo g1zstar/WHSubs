5e0aaa4545460502bef800b75a9f879423008d0b
g1zstar <sensiblemagic@gmail.com>
2017-11-06 20:13:57 -0500

    testing

--- WHSubs.lua
+++ WHSubs.lua
@@ -1,24 +1,25 @@
-sub_alcovex    = {["ROGUE"] = 99991231}
-sub_evilgav    = {["ROGUE"] = 20171127}
-sub_fooshi     = {["ROGUE"] = 20171127}
-sub_toxin87    = {["ROGUE"] = 20171127}
-sub_zrrx       = {["ROGUE"] = 20171127}
-sub_poisonling = {["ROGUE"] = 20171128}
-sub_mxrender   = {["ROGUE"] = 20171129}
-sub_spacek     = {["ROGUE"] = 20171129}
-sub_inkedqt    = {["ROGUE"] = 20171129}
-sub_binkya     = {["ROGUE"] = 20171201}
+sub_g1zstar        = {["ALL"]   = 99991231}
+sub_alcovex        = {["ROGUE"] = 99991231}
+sub_evilgav        = {["ROGUE"] = 20171127}
+sub_fooshi         = {["ROGUE"] = 20171127}
+sub_toxin87        = {["ROGUE"] = 20171127}
+sub_zrrx           = {["ROGUE"] = 20171127}
+sub_poisonling     = {["ROGUE"] = 20171128}
+sub_mxrender       = {["ROGUE"] = 20171129}
+sub_spacek         = {["ROGUE"] = 20171129}
+sub_inkedqt        = {["ROGUE"] = 20171129}
+sub_binkya         = {["ROGUE"] = 20171201}
 sub_frontliner1989 = {["ROGUE"] = 20171202}
-sub_neverlucky = {["ROGUE"] = 20171202}
+sub_neverlucky     = {["ROGUE"] = 20171202}
 
-sub_DEATHKNIGHT = 20171231
-sub_DEMONHUNTER = 20171231
-sub_DRUID = 20171231
-sub_HUNTER = 20171231
-sub_MAGE = 20171231
-sub_MONK = 20171231
-sub_PALADIN = 20171231
-sub_PRIEST = 20171231
-sub_SHAMAN = 20171231
-sub_WARLOCK = 20171231
-sub_WARRIOR = 20171231
+sub_DEATHKNIGHT    = 20171231
+sub_DEMONHUNTER    = 20171231
+sub_DRUID          = 20171231
+sub_HUNTER         = 20171231
+sub_MAGE           = 20171231
+sub_MONK           = 20171231
+sub_PALADIN        = 20171231
+sub_PRIEST         = 20171231
+sub_SHAMAN         = 20171231
+sub_WARLOCK        = 20171231
+sub_WARRIOR        = 20171231
