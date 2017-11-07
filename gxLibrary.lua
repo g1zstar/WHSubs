58d423ed200fef73475d34d4b60d63504f919d9e
g1zstar <sensiblemagic@gmail.com>
2017-11-06 21:49:37 -0500

    TEST

--- gxLibrary.lua
+++ gxLibrary.lua
@@ -1,9 +1,11 @@
 --[[function math.sign(v)]]
-_G["math"].sign = function(v)
+-- _G["math"].sign = function(v)
+local function sign(v)
     return (v >= 0 and 1) or -1
 end
 --[[function math.round(v, bracket)]]
-_G["math"].round = function(v, bracket)
+-- _G["math"].round = function(v, bracket)
+local function round(v, bracket)
     bracket = bracket or 1
     return math.floor(v/bracket + math.sign(v) * 0.5) * bracket
 end
